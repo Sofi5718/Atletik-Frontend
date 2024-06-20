52c6ae949a3681a4a58ad796cf8465c9caf1a755
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-06-20 15:28:28 +0200

    Deltagerliste

---
+++ src/Componets/DeltagerList.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { getDeltagere } from "../services/apiFacade";
+import { Deltager } from "../services/entityFacade";
+import Form from "./Form";
+
+export default function DeltagerList() {
+    const [deltagere, setDeltagere] = useState<Deltager[]>([]);
+
+    useEffect(() => {
+        getDeltagere().then((data) => setDeltagere(data));
+    }, []);
+
+    return (
+        <div className="flex flex-col items-center justify-center p-5 bg-gray-200 m-auto">
+            <table className="text-center">
+                <thead>
+                    <tr>
+                        <th>Navn</th>
+                        <th>Alder</th>
+                        <th>Køn</th>
+                        <th>Klub</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {deltagere.map((deltager) => (
+                        <tr key={deltager.id}>
+                            <td>{deltager.navn}</td>
+                            <td>{deltager.alder}</td>
+                            <td>{deltager.køn}</td>
+                            <td>{deltager.klub}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <Form />
+        </div>
+    );
+}
