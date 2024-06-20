94901fd7c108cb024f5825a7e9319f796241f3e4
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-06-20 19:18:06 +0200

    søge function

--- src/Componets/DeltagerList.tsx
+++ src/Componets/DeltagerList.tsx
@@ -5,13 +5,32 @@ import { Link } from "react-router-dom";
 
 export default function DeltagerList() {
     const [deltagere, setDeltagere] = useState<Deltager[]>([]);
+const [originalDeltagere, setOriginalDeltagere] = useState<Deltager[]>([]);
 
     useEffect(() => {
-        getDeltagere().then((data) => setDeltagere(data));
+        getDeltagere().then((data) => {
+            setDeltagere(data);
+            setOriginalDeltagere(data);
+        });
     }, []);
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const searchValue = e.target.value.toLowerCase();
+        if (searchValue === "") {
+            setDeltagere(originalDeltagere);
+        } else {
+            const searchResults = originalDeltagere.filter((deltager) => {
+                return deltager.navn.toLowerCase().includes(searchValue) || deltager.klub.toLowerCase().includes(searchValue);
+            });
+            setDeltagere(searchResults);
+        }
+    };
 
     return (
         <div className="flex flex-col items-center justify-center p-5 bg-gray-200 m-auto">
+            <div>
+                <input type="text" placeholder="Søg" onChange={handleSearch} />
+            </div>
             <table className="text-center">
                 <thead>
                     <tr>
